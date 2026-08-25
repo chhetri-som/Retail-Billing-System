0b8e89d11b6f895a20028ab612ba877c954a63f3
Anish.Chhetri <chhetri.anish14@gmail.com>
2026-08-25 15:29:30 +0530

    create form for manage category

--- frontend/src/pages/ManageCategory/ManageCategory.jsx
+++ frontend/src/pages/ManageCategory/ManageCategory.jsx
@@ -1,13 +1,15 @@
 import './ManageCategory.css';
+import CategoryForm from "../../components/CategoryForm/CategoryForm.jsx";
+import CategoryList from "../../components/CategoryList/CategoryList.jsx";
 
 const ManageCategory = () => {
     return (
         <div className="category-container text-light">
             <div className="left-column">
-                category form
+                <CategoryForm />
             </div>
             <div className="right-column">
-                list of categories
+                <CategoryList />
             </div>
         </div>
     )
