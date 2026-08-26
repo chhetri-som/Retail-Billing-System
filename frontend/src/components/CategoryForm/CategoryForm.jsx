a324b6834ab9a1693820e993ac3d21fdec25a2dd
Anish.Chhetri <chhetri.anish14@gmail.com>
2026-08-26 22:26:47 +0530

    add category form and connect to api

--- frontend/src/components/CategoryForm/CategoryForm.jsx
+++ frontend/src/components/CategoryForm/CategoryForm.jsx
@@ -1,15 +1,72 @@
+import {useContext, useState} from "react";
+import {assets} from "../../assets/assets.js";
+import toast from "react-hot-toast";
+import {addCategory} from "../../Service/CategoryService.js";
+import {AppContext} from "../../context/AppContext.jsx";
+
 const CategoryForm = () => {
+
+    const {categories, setCategories} = useContext(AppContext);
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        bgColor: "#2c2c2c",
+    });
+
+//    useEffect(() => {
+//        console.log(data);
+//    }, [data]);
+
+    const onChangeHandler = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setData((data) => ({...data, [name]: value}));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        if (!image) {
+            toast.error("Replace upload icon with image.")
+            return;
+        }
+        setLoading(true);
+        const formData = new FormData();
+        formData.append("category", JSON.stringify(data));
+        formData.append("file", image);
+        try {
+            const response = await addCategory(formData);
+            if (response.status == 201) {
+                setCategories([...categories, response.data]);
+                toast.success("Category added successfully.");
+                setData({
+                    name: "",
+                    description: "",
+                    bgColor: "2c2c2c"
+                });
+                setImage(false);
+            }
+        } catch (err) {
+            console.error(err);
+            toast.error("Error creating category.");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+
     return (
         <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className="card-body">
-                        <form>
+                        <form onSubmit={onSubmitHandler}>
                             <div className="mb-3">
                                 <label htmlFor="image" className="form-label">
-                                    <img src="https://placehold.co/48x48" alt="" width={48}/>
+                                    <img src={image ? URL.createObjectURL(image) : assets.upload} alt="upload icon" width={48}/>
                                 </label>
-                                <input type="file" name="image" id="image" className='form-control' hidden />
+                                <input type="file" name="image" id="image" className='form-control' hidden onChange={(e) => setImage(e.target.files[0])}/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
@@ -19,6 +76,8 @@ const CategoryForm = () => {
                                     id="name"
                                     className="form-control"
                                     placeholder="Category Name"
+                                    onChange={onChangeHandler}
+                                    value={data.name}
                                />
                             </div>
                             <div className="mb-3">
@@ -29,6 +88,8 @@ const CategoryForm = () => {
                                     id="description"
                                     className="form-control"
                                     placeholder="Write content here.."
+                                    onChange={onChangeHandler}
+                                    value={data.description}
                                 ></textarea>
                             </div>
                             <div className="mb-3">
@@ -39,9 +100,11 @@ const CategoryForm = () => {
                                     name="bgColor"
                                     id="bgcolor"
                                     placeholder="#ffffff"
+                                    onChange={onChangeHandler}
+                                    value={data.bgColor}
                                 />
                             </div>
-                            <button type="submit" className="btn btn-warning w-100">Save</button>
+                            <button type="submit" disabled={loading} className="btn btn-warning w-100">{loading ? "Loading..." : "Submit"}</button>
                         </form>
                     </div>
                 </div>
