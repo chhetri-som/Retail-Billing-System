5c5a49ef9ad48550db3d9f13247652968d26c405
Anish.Chhetri <chhetri.anish14@gmail.com>
2026-09-06 01:12:05 +0530

    build the items dashboard, add items, delete items, update count of items in category, set context

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,17 +1,21 @@
 import {createContext, useEffect, useState} from "react";
 import {fetchCategories} from "../Service/CategoryService.js";
+import {fetchItems} from "../Service/ItemService.js";
 
 export const AppContext = createContext(null);
 
 export const AppContextProvider = (props) => {
 
     const [categories, setCategories] = useState([]);
+    const [itemsData, setItemsData] = useState([]);
     const [auth, setAuth] = useState({token: null, role: null});
 
     useEffect(() => {
         async function loadData() {
             const response = await fetchCategories();
+            const itemResponse = await fetchItems();
             setCategories(response.data);
+            setItemsData(itemResponse.data);
         }
         loadData();
     }, []);
@@ -24,7 +28,9 @@ export const AppContextProvider = (props) => {
         categories,
         setCategories,
         auth,
-        setAuthData
+        setAuthData,
+        itemsData,
+        setItemsData,
     }
 
     return <AppContext.Provider value={contextValue}>
