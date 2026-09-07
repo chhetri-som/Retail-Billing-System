c255c698726892076ec678d920148397e1b726bf
Anish.Chhetri <chhetri.anish14@gmail.com>
2026-09-07 16:31:41 +0530

    add items in explore page and link them to categories

--- frontend/src/components/DisplayItems/DisplayItems.jsx
+++ frontend/src/components/DisplayItems/DisplayItems.jsx
@@ -1,14 +1,29 @@
 import './DisplayItems.css'
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {AppContext} from "../../context/AppContext.jsx";
 import Item from "../Item/Item.jsx";
+import SearchBox from "../SearchBox/SearchBox.jsx";
 
-const DisplayItems = () => {
+const DisplayItems = ({selectedCategory}) => {
     const {itemsData} = useContext(AppContext);
+    const [searchText, setSearchText] = useState("");
+    const filteredItems = itemsData.filter(item => {
+        if (!selectedCategory) return true;
+        return item.categoryId === selectedCategory;
+    }).filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()));
+
     return (
         <div className="p-3">
+            <div className="d-flex justify-content-between align-items-center mb-4">
+                <div>
+
+                </div>
+                <div>
+                    <SearchBox onSearch={setSearchText}></SearchBox>
+                </div>
+            </div>
             <div className="row g-3">
-                {itemsData.map((item, index) => (
+                {filteredItems.map((item, index) => (
                     <div key={index} className="col-md-4 col-sm-6">
                         <Item
                             itemName={item.name}
